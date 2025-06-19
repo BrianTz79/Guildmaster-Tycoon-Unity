6cf65c0523dfe49ea13a0497f38fb32600921f90
Brian Tellez <brian.tellez79@gmail.com>
2025-06-19 11:38:26 -0700

    Initial commit: Configuración inicial del proyecto Unity

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("¡El GameManager se ha iniciado! ¡Todo está conectado!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
